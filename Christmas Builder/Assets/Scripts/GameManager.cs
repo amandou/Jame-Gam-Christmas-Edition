a9bd37ec15e9710ec3048a6ff531acbc5aa03723
Amandou <amanda.lrib@gmail.com>
2022-12-21 16:25:06 -0300

    Implement function to draw cards on the table when the game started.

--- Christmas Builder/Assets/Scripts/GameManager.cs
+++ Christmas Builder/Assets/Scripts/GameManager.cs
@@ -11,21 +11,38 @@ public class GameManager : MonoBehaviour
     public bool[] availableCardSlots;
     public TextMeshProUGUI deckSizeText;
 
-    public void DrawCard ()
-    { 
+
+    public void Start()
+    {
+        DrawCardsOnTheTable();
+    }
+
+    public void DrawCard()
+    {
         if (deck.Count >= 1)
         {
-            Card card = deck[Random.Range(0,deck.Count)];
+            Card card = deck[Random.Range(0, deck.Count)];
             for (int i = 0; i < availableCardSlots.Length; i++)
             {
                 if (availableCardSlots[i])
                 {
-                    card.gameObject.SetActive(true);
-                    Vector3 cardPosition = cardSlots[i].position;
-                    card.transform.position = cardPosition;
-                    availableCardSlots[i] = false;
-                    deck.Remove(card);
+                    ShowCard(card, deck, i);
                     return;
+                }
+            }
+        }
+    }
+
+    public void DrawCardsOnTheTable()
+    {
+        if (deck.Count >= 1)
+        {
+            for (int i = 0; i < availableCardSlots.Length; i++)
+            {
+                if (availableCardSlots[i])
+                {
+                    Card card = deck[Random.Range(0, deck.Count)];
+                    ShowCard(card, deck, i);
                 }
             }
         }
@@ -34,5 +51,14 @@ public class GameManager : MonoBehaviour
     private void Update()
     {
         deckSizeText.text = deck.Count.ToString();
+    }
+
+    private void ShowCard(Card card, List<Card> deck, int index)
+    {
+        card.gameObject.SetActive(true);
+        Vector3 cardPosition = cardSlots[index].position;
+        card.transform.position = cardPosition;
+        availableCardSlots[index] = false;
+        deck.Remove(card);
     }
 }
